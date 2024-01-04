19c572d80265c88671ab162fbb18a51a6cf8f705
Allan Moreno <allan@Allans-MacBook-Pro.local>
2024-01-03 18:18:07 -0600

    release popup

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-displayPoints()
-displayScore()
-displayStatus()
+displayPoints();
+displayScore();
+displayStatus();
 
 window.addEventListener('click', function(event) {
-    if (event.target.matches('[data-action="reset"]')) {
-        const target = event.target.getAttribute('data-target');
-        reset(target);
-    }
+	if (event.target.matches('[data-action="reset"]')) {
+		const target = event.target.getAttribute('data-target');
+		reset(target);
+	}
 
-    if (event.target.matches('[data-action="print"]')) {
-        window.print();
-    }
+	if (event.target.matches('[data-action="print"]')) {
+		window.print();
+	}
 
-    if (event.target.matches('[data-action="dialog"]')) {
-        const target = event.target.getAttribute('data-target');
-        const dialog = document.querySelector(`#${target}`);
-        dialog.showModal();
-    }
+	if (event.target.matches('[data-action="dialog"]')) {
+		const target = event.target.getAttribute('data-target');
+		const dialog = document.querySelector(`#${target}`);
+		dialog.showModal();
+	}
 
-    if (event.target.matches('[data-action="dialog-close"]')) {
-        const target = event.target.getAttribute('data-target');
-        const dialog = document.querySelector(`#${target}`);
-        dialog.close();
-    }
-})
+	if (event.target.matches('[data-action="dialog-close"]')) {
+		const target = event.target.getAttribute('data-target');
+		const dialog = document.querySelector(`#${target}`);
+		dialog.close();
+	}
+});
+
+/**
+ * ----------------
+ * search
+ * ----------------
+ */
+(() => {
+	const searchInput = document.querySelector('#search-value');
+	if (searchInput === null) return;
+	searchInput.addEventListener('input', function(event) {
+		const searchValue = searchInput.value.trim().toLowerCase();
+		console.log(searchValue);
+		const list = document.querySelectorAll('#dictionary dt');
+		list.forEach(function(item) {
+			const term = item.innerText.toLowerCase();
+			const description = item.nextElementSibling.innerText.toLowerCase();
+			if (term.includes(searchValue)) {
+				item.style.display = ''; // term
+				item.nextElementSibling.style.display = ''; // description
+			} else {
+				item.style.display = 'none';
+				item.nextElementSibling.style.display = 'none';
+			}
+		});
+	});
+})();
+
 
 /**
  * --------------
@@ -32,35 +59,54 @@ window.addEventListener('click', function(event) {
  */
 
 class PopUp extends HTMLElement {
-    constructor() {
-        super()
-    }
-    connectedCallback() {
-        this.innerHTML = `
-        <div class="popup box-shadow border-radius-0.5">
-            <div class="flex gap-1">
-                <div class="width-min">
-                    <img class="border-radius-round"
-                         src="/istqb-ctfl/img/tutor.png"
-                         alt="Tutor smiling" width="60">
-                </div>
-                <div class="width-max font-size-small">
-                    <p>Estudiar solo es difícil.</p>
-                    <p>Únete a un grupo de estudio y recibe ayuda de un tutor.</p>
-                    <p>Aprende fundamentos, automatización, y desarrollo web.</p>
-                    <a class="font-size-small text-uppercase" href="#!">Más Info Aquí</a>
-                </div>
-            </div>
-        </div>
-        `
-    }
+	constructor() {
+		super()
+	}
+	connectedCallback() {
+		this.innerHTML = `
+		<div id="needhelp" class="popup box-shadow border-radius-0.5">
+			<div class="flex gap-1">
+				<div class="width-min">
+					<img class="border-radius-round"
+						 src="/istqb-ctfl/img/tutor.png"
+						 alt="Tutor smiling" width="60">
+				</div>
+				<div class="width-max font-size-small">
+					<p><strong>Need help?</strong></p>
+					<p>Si tienes dudas, preguntas, o sugerencias, escríbeme a: allanmoreno@mail.com</p>
+					<!--<p>Estudiar solo es difícil.</p>-->
+					<!--<p>Únete a un grupo de estudio y recibe ayuda de un tutor.</p>-->
+					<!--<p>Aprende fundamentos, automatización, y desarrollo web.</p>-->
+					<!--<a class="font-size-small text-uppercase" href="#!">Más Info Aquí</a>-->
+					<button class="btn-small text-uppercase" data-action="close" data-target="#needhelp" type="button">Cerrar</button>
+				</div>
+			</div>
+		</div>
+		`;
+		const closeButton = this.querySelector('[data-action="close"]');
+		closeButton.closest('button').addEventListener('click', function(event) {
+		    sessionStorage.setItem('needhelp', 'dismiss')
+		    document.querySelector('#needhelp').style.display = 'none';
+		});
+	}
 }
 
 customElements.define('ui-popup', PopUp);
+setTimeout(() => {
+    if (sessionStorage.getItem('needhelp') === 'dismiss') return;
+	const popup = document.createElement('ui-popup');
+	document.body.insertAdjacentElement('beforeEnd', popup);
+}, 5000);
 
-// inject popup manually for better control
-if (window.location.href.includes('assessment')) {
-    // document.body.innerHTML += `<ui-popup></ui-popup>`;
-}
 
-document.body.innerHTML += `<script type="text/javascript">var sc_project=12954337; var sc_invisible=1; var sc_security="4c7965b8";</script><script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>`
+((doc, script) => {
+	var sc_project=12954337;
+	var sc_invisible=1;
+	var sc_security="4c7965b8";
+	script = doc.createElement('script');
+	script.type = 'text/javascript';
+	script.async = true;
+	script.onload = function() { /* remote script has loaded */ };
+	script.src = 'https://www.statcounter.com/counter/counter.js';
+	doc.getElementsByTagName('head')[0].appendChild(script);
+})(document);
